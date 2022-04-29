e6c8acfbb3af2335838a18a11c7ddb3cad0dfaff
huy <huyenthoaiphongvan19@gmail.com>
2022-04-29 09:28:58 +0700

    sc commit

--- index.py
+++ index.py
@@ -6,6 +6,65 @@ from pathlib import Path
 import base64
 import os
 
+import cv2
+import io
+from PIL import Image
+import torch
+from werkzeug.exceptions import BadRequest
+
+# https://docs.ovh.com/asia/en/publiccloud/ai/training/web-service-yolov5/
+class LogoDetection(tornado.web.RequestHandler):
+    async def get(self):
+        self.render("template/index.py")
+
+    async def post(self):
+        file = extract_img(request)
+        img_bytes = file.read()
+        # choice of the model
+        results = LogoDetection.get_prediction(img_bytes,dictOfModels[request.form.get("model_choice")])
+        print(f'User selected model : {request.form.get("model_choice")}')
+        # updates results.imgs with boxes and labels
+        results.render()
+        # encoding the resulting image and return it
+        for img in results.imgs:
+          RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+          im_arr = cv2.imencode('.jpg', RGB_img)[1]
+          response = make_response(im_arr.tobytes())
+          response.headers['Content-Type'] = 'image/jpeg'
+        # return your image with boxes and labels
+        return response
+
+    async def extract_img(request):
+        # checking if image uploaded is valid
+        if 'file' not in request.files:
+          raise BadRequest("Missing file parameter!")
+        file = request.files['file']
+        if file.filename == '':
+          raise BadRequest("Given file is invalid")
+        return file
+
+    async def get_prediction(img_bytes,model):
+        img = Image.open(io.BytesIO(img_bytes))
+        # inference
+        results = model(img, size=640)  
+        return results
+
+    async def loadweight():
+        # create a python dictionary for your models d = {<key>: <value>, <key>: <value>, ..., <key>: <value>}
+        dictOfModels = {}
+        # create a list of keys to use them in the select part of the html code
+        listOfKeys = []
+        for r, d, f in os.walk("models_train"):
+          for file in f:
+           if ".pt" in file:
+            # example: file = "model1.pt"
+            # the path of each model: os.path.join(r, file)
+            dictOfModels[os.path.splitext(file)[0]] = torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join(r, file), force_reload=True)
+            # you would obtain: dictOfModels = {"model1" : model1 , etc}
+          for key in dictOfModels :
+            listOfKeys.append(key)     # put all the keys in the listOfKeys
+          print(listOfKeys)
+
 class uploadImgHandler(tornado.web.RequestHandler):
     
     async def get(self):
